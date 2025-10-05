20c4d9b4bc5d0990185b0b7f72e2f6a29f7ce9ec
anggarasa <anggasaputra6259@gmail.com>
2025-10-05 07:26:59 +0700

    delete toast notification

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,6 +1,5 @@
 <x-layouts.app.sidebar :title="$title ?? null">
     <flux:main>
-        @livewire('notifications.realtime-notifier')
         {{ $slot }}
     </flux:main>
 </x-layouts.app.sidebar>
