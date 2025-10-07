7393466bdaf56cdee9b3f1654e3dfd1a7d3a7b4a
anggarasa <anggasaputra6259@gmail.com>
2025-10-07 11:28:29 +0700

    add setup vercel

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
     ],
     server: {
         cors: true,
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: "assets/[name]-[hash][extname]",
+                chunkFileNames: "assets/[name]-[hash].js",
+                entryFileNames: "assets/[name]-[hash].js",
+            },
+        },
     },
 });
